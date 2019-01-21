fed3c0eb6428974c7cb27ece41baa012db066fe1
Jasonarea <hyeonsuns@naver.com>
2019-01-22 02:57:13 +0900

    #9- Timer 시작, 정지, 리셋 기능 구현

--- app/src/main/java/com/nexters/ticktock/MainActivity.kt
+++ app/src/main/java/com/nexters/ticktock/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.nexters.ticktock
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.nexters.ticktock.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
