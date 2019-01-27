88cfd34848a07b9be288757d8004faa50e9a1098
rkddlsgur983 <rkddlsgur983@naver.com>
2019-01-27 19:07:24 +0900

    feat: #23-apply google autocomplete custom view

--- app/src/main/java/com/nexters/ticktock/AutoCompleteActivity.kt
+++ app/src/main/java/com/nexters/ticktock/AutoCompleteActivity.kt
@@ -1,58 +1,60 @@
 package com.nexters.ticktock
 
 import android.app.Activity
-import android.content.DialogInterface
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.app.AlertDialog
+import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.TextView
-import com.google.android.gms.common.GooglePlayServicesNotAvailableException
-import com.google.android.gms.common.GooglePlayServicesRepairableException
-import com.google.android.gms.common.api.Status
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.PendingResult
+import com.google.android.gms.common.api.ResultCallback
 import com.google.android.gms.location.places.AutocompleteFilter
-import com.google.android.gms.location.places.Place
-import com.google.android.gms.location.places.ui.PlaceAutocomplete
+import com.google.android.gms.location.places.PlaceBuffer
+import com.google.android.gms.location.places.Places
 import com.google.android.gms.maps.model.LatLng
 import com.nexters.ticktock.databinding.ActivityAutoCompleteBinding
 import com.odsay.odsayandroidsdk.API
 import com.odsay.odsayandroidsdk.ODsayData
 import com.odsay.odsayandroidsdk.ODsayService
 import com.odsay.odsayandroidsdk.OnResultCallbackListener
-import kotlinx.android.synthetic.main.activity_auto_complete.*
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
+import java.lang.Exception
+import kotlin.collections.ArrayList
 
 
-class AutoCompleteActivity : AppCompatActivity(), View.OnClickListener, DialogInterface.OnClickListener, OnResultCallbackListener {
+class AutoCompleteActivity : AppCompatActivity(), View.OnClickListener, OnResultCallbackListener, PlaceAutocompleteAdapter.PlaceAutoCompleteInterface, GoogleApiClient.OnConnectionFailedListener,
+        GoogleApiClient.ConnectionCallbacks, TextWatcher {
 
-    private lateinit var binding: ActivityAutoCompleteBinding
+    private lateinit var binding: ActivityAutoCompleteBinding // 데이터 바인딩
     private val TAG:String = "AutoCompleteActivity"
 
     private val GPS_ENABLE_REQUEST_CODE = 2001
 
-    private val PLACE_AUTOCOMPLETE_REQUEST_CODE_FROM = 1;
-    private val PLACE_AUTOCOMPLETE_REQUEST_CODE_TO = 2;
-
-    private var isFrom:Boolean = true
-
-    private var checkNumberFrom = 0
-    private var checkNumberTo = 0
+    private var latLng:LatLng? = null
 
     private var fromLatLng:LatLng? = null
     private var toLatLng:LatLng? = null
 
-    private lateinit var geocoder: Geocoder
-    private lateinit var gps: GPSInfo
+    private lateinit var geocoder: Geocoder // 좌표 - 주소 변환
+    private lateinit var gps: GPSInfo // gps
 
-    private lateinit var odsayService: ODsayService
+    private lateinit var googleApiClient: GoogleApiClient // 구글 검색 api 사용을 위함
+    private lateinit var placeAutocompleteAdapter: PlaceAutocompleteAdapter // 리사이클러뷰 어댑터
+
+    private lateinit var odsayService: ODsayService // 오디세이
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -70,70 +72,28 @@ class AutoCompleteActivity : AppCompatActivity(), View.OnClickListener, DialogIn
         // 데이터 획득 제한 시간(단위(초), default : 5초)
         odsayService.setConnectionTimeout(5000);
 
-        binding.layoutAutoFrom.setOnClickListener(this)
-        binding.layoutAutoTo.setOnClickListener(this)
-        binding.btnShowLocation.setOnClickListener(this)
+        binding.btnSaveData.setOnClickListener(this)
+
+        initPlace()
+    }
+
+    // 구글 클라이언트 연결
+    override fun onStart() {
+        googleApiClient.connect()
+        super.onStart()
+    }
+
+    override fun onStop() {
+        googleApiClient.disconnect()
+        super.onStop()
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
 
         super.onActivityResult(requestCode, resultCode, data)
-
-        when (requestCode){
+        when (requestCode) {
+            // gps 설정 변경 후 재연결
             GPS_ENABLE_REQUEST_CODE -> gps = GPSInfo(this)
-
-            PLACE_AUTOCOMPLETE_REQUEST_CODE_FROM -> {
-                when (resultCode) {
-                    Activity.RESULT_OK -> {
-                        val place:Place = PlaceAutocomplete.getPlace(this, data)
-                        binding.tvCurrent.text = "${place.name}"
-
-                        fromLatLng = LatLng(place.latLng.latitude, place.latLng.longitude)
-                    }
-                    PlaceAutocomplete.RESULT_ERROR -> {
-                        val status:Status = PlaceAutocomplete.getStatus(this, data)
-                        Log.i(TAG, status.statusMessage)
-                    }
-                    Activity.RESULT_CANCELED -> {
-                        // 사용자 입력에의한 종료
-                    }
-                }
-            }
-
-            PLACE_AUTOCOMPLETE_REQUEST_CODE_TO -> {
-                when (resultCode) {
-                    Activity.RESULT_OK -> {
-                        val place: Place = PlaceAutocomplete.getPlace(this, data)
-                        binding.tvDestination.text = "${place.name}"
-
-                        toLatLng = LatLng(place.latLng.latitude, place.latLng.longitude)
-                    } PlaceAutocomplete.RESULT_ERROR -> {
-                        val status: Status = PlaceAutocomplete.getStatus(this, data)
-                        Log.i(TAG, status.statusMessage)
-                    } Activity.RESULT_CANCELED -> {
-                        // 사용자 입력에의한 종료
-                    }
-                }
-            }
-        }
-    }
-
-    // 자동완성 액티비티 생성
-    private fun createAutoComplete(requestCode: Int) {
-
-        try {
-            val typeFilter:AutocompleteFilter = AutocompleteFilter.Builder().setCountry("KR").build()
-
-            val intent:Intent = PlaceAutocomplete
-                                .IntentBuilder(PlaceAutocomplete.MODE_OVERLAY)
-                                .setFilter(typeFilter)
-                                .build(this)
-
-            startActivityForResult(intent, requestCode)
-        } catch (e: GooglePlayServicesRepairableException) {
-            e.printStackTrace()
-        } catch (e: GooglePlayServicesNotAvailableException) {
-            e.printStackTrace()
         }
     }
 
@@ -163,6 +123,7 @@ class AutoCompleteActivity : AppCompatActivity(), View.OnClickListener, DialogIn
         return address
     }
 
+    // TODO 인자변경
     private fun getGPSLocation(textView: TextView) {
         if (gps.isGetLocation) {
             var latitude = gps.latitude
@@ -178,36 +139,99 @@ class AutoCompleteActivity : AppCompatActivity(), View.OnClickListener, DialogIn
             }
 
             // textView.text = "${address.getAddressLine(0).substring(4)}" // (대한민국) 서울시~~
-            textView.text = "현위치"
-            if(isFrom)
-                fromLatLng = LatLng(latitude, longitude)
-            else
-                toLatLng = LatLng(latitude, longitude)
+            // TODO
+            latLng = LatLng(latitude, longitude)
         } else {
             textView.text = "잠시뒤 다시 시도해주세요"
             Log.d(TAG, "cannot find")
         }
     }
 
+    /*
+     * 구글 검색 api 사용을 위한 메소드 시작
+     */
+    fun initPlace() {
+        googleApiClient = GoogleApiClient
+                .Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Places.GEO_DATA_API)
+                .build()
+
+        binding.listSearch.setHasFixedSize(true)
+        binding.listSearch.layoutManager = LinearLayoutManager(this)
+
+        val typeFilter = AutocompleteFilter.Builder()
+                .setCountry("KR")
+                .build()
+
+        placeAutocompleteAdapter = PlaceAutocompleteAdapter(this, R.layout.item_search, googleApiClient, null, typeFilter)
+        binding.listSearch.adapter = placeAutocompleteAdapter
+
+        binding.edSearchFrom.addTextChangedListener(this)
+        binding.edSearchTo.addTextChangedListener(this)
+    }
+
+    override fun onPlaceClick(resultList: ArrayList<PlaceAutocompleteAdapter.PlaceAutocomplete>, position: Int) {
+        if(resultList.size > 0) {
+            try {
+                val placeId: String = resultList.get(position).placeId.toString()
+                val placeResult: PendingResult<PlaceBuffer> = Places.GeoDataApi.getPlaceById(googleApiClient, placeId)
+                placeResult.setResultCallback {
+                    if(it.count == 1) {
+                        // 이곳에서 키워드를 선택한 데이터를 처리한다.
+                        val location = Location(it.get(0).name.toString())
+                        location.latitude = it.get(0).latLng.latitude
+                        location.longitude = it.get(0).latLng.longitude
+                    } else {
+                        // error
+                    }
+                }
+            } catch (e: Exception) {
+
+            } finally {
+                runOnUiThread { run {
+                    placeAutocompleteAdapter.clearList()
+                    binding.listSearch.setVisibility(View.GONE)
+                } }
+            }
+        }
+    }
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+    }
+
+    override fun onConnected(p0: Bundle?) {
+    }
+
+    override fun onConnectionSuspended(p0: Int) {
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+    }
+
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        if (count > 0) {
+            binding.listSearch.setVisibility(View.VISIBLE)
+        } else {
+            placeAutocompleteAdapter.clearList()
+            binding.listSearch.setVisibility(View.GONE)
+        }
+        if (!s.toString().equals("") && googleApiClient.isConnected) {
+            placeAutocompleteAdapter.filter.filter(s.toString())
+        } else if (!googleApiClient.isConnected) {
+            Log.e("", "NOT CONNECTED")
+        }
+    }
+    /*
+    * 종료
+    */
+
     override fun onClick(v: View?) {
         when (v?.id) {
-            binding.layoutAutoFrom.id -> {
-                isFrom = true
-                val builder = AlertDialog.Builder(this)
-                val items = arrayOf("현위치", "검색")
-                builder.setSingleChoiceItems(items, checkNumberFrom, this)
-                builder.create().show()
-            }
-
-            binding.layoutAutoTo.id -> {
-                isFrom = false
-                val builder = AlertDialog.Builder(this)
-                val items = arrayOf("현위치", "검색")
-                builder.setSingleChoiceItems(items, checkNumberTo, this)
-                builder.create().show()
-            }
-
-            binding.btnShowLocation.id -> {
+            binding.btnSaveData.id -> {
                 odsayService.requestSearchPubTransPath(
                         fromLatLng!!.longitude.toString(),
                         fromLatLng!!.latitude.toString(),
@@ -221,27 +245,9 @@ class AutoCompleteActivity : AppCompatActivity(), View.OnClickListener, DialogIn
         }
     }
 
-    override fun onClick(dialog: DialogInterface?, index: Int) {
-
-        dialog?.dismiss() // 클릭시 다이얼로그 사라짐
-
-        if (isFrom) {
-            checkNumberFrom = index
-            when (index) {
-                0 -> getGPSLocation(binding.tvCurrent)
-
-                1 -> createAutoComplete(PLACE_AUTOCOMPLETE_REQUEST_CODE_FROM)
-            }
-        } else {
-            checkNumberTo = index
-            when (index) {
-                0 -> getGPSLocation(binding.tvDestination)
-
-                1 -> createAutoComplete(PLACE_AUTOCOMPLETE_REQUEST_CODE_TO)
-            }
-        }
-    }
-
+    /*
+     * 오디세이 api 사용을 위한 메소드 시작
+     */
     override fun onSuccess(odsayData: ODsayData, api: API?) {
         try {
             // API Value 는 API 호출 메소드 명을 따라갑니다.
@@ -263,4 +269,9 @@ class AutoCompleteActivity : AppCompatActivity(), View.OnClickListener, DialogIn
     override fun onError(i: Int, s: String?, api: API?) {
         if (api == API.SEARCH_PUB_TRANS_PATH) {}
     }
+    /*
+     * 종료
+     */
 }
+
+
---
+++ app/src/main/java/com/nexters/ticktock/PlaceAutocompleteAdapter.kt
@@ -0,0 +1,167 @@
+package com.nexters.ticktock
+
+import android.content.Context
+import android.graphics.Typeface
+import android.support.constraint.ConstraintLayout
+import android.support.v7.widget.RecyclerView
+import android.text.style.CharacterStyle
+import android.text.style.StyleSpan
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
+import android.widget.RelativeLayout
+import android.widget.TextView
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.common.api.PendingResult
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.location.places.AutocompleteFilter
+import com.google.android.gms.location.places.AutocompletePrediction
+import com.google.android.gms.location.places.AutocompletePredictionBuffer
+import com.google.android.gms.location.places.Places
+import com.google.android.gms.maps.model.LatLngBounds
+import java.util.concurrent.TimeUnit
+
+
+class PlaceAutocompleteAdapter(
+        private val context: Context,
+        private val resource:Int,
+        private val googleApiClient: GoogleApiClient,
+        private var bounds:LatLngBounds?,
+        private var filter:AutocompleteFilter?
+): RecyclerView.Adapter<PlaceAutocompleteAdapter.PlaceViewHolder>(), Filterable{
+
+    interface PlaceAutoCompleteInterface {
+        fun onPlaceClick(resultList:ArrayList<PlaceAutocomplete> , position:Int)
+    }
+
+    private var listener:PlaceAutoCompleteInterface
+    private val TAG = "PLACE_AUTOCOMPLETE_ADAPTER"
+    private var resultList:ArrayList<PlaceAutocomplete> = ArrayList()
+
+    init {
+        listener = context as PlaceAutoCompleteInterface
+    }
+
+    fun clearList() {
+        if(resultList.size > 0) {
+            resultList.clear()
+        }
+    }
+
+    override fun getFilter(): Filter {
+
+        return object : Filter() {
+
+            override fun performFiltering(constraint: CharSequence?): FilterResults {
+
+                val results = FilterResults()
+                // 제약 조건이 주어지지 않으면 자동 완성 쿼리를 건너 뜁니다.
+                if (constraint != null) {
+                    //(제약) 검색 문자열에 대한 자동 완성 API를 쿼리하십시오.
+                    resultList = getAutocomplete(constraint)
+
+                    if (resultList.size > 0) {
+                        //API가 성공적으로 결과를 반환했습니다.
+                        results.values = resultList
+                        results.count = resultList.size
+                    }
+                }
+
+                return results
+            }
+
+            override fun publishResults(constraint: CharSequence, results: FilterResults?) {
+
+                if (results != null && results.count > 0) {
+                    //API가 하나 이상의 결과를 반환하고 데이터를 업데이트합니다.
+                    notifyDataSetChanged()
+                } else {
+                    //API가 결과를 반환하지 않았고 데이터 세트를 무효화
+                    //notifyDataSetInvalidated();
+                }
+            }
+        }
+    }
+
+    private fun getAutocomplete(constraint: CharSequence): ArrayList<PlaceAutocomplete> {
+        if(googleApiClient.isConnected) {
+            // 자동 완성 API에 쿼리를 제출하고 PendingResult를 검색합니다.
+            // 쿼리가 완료되면 결과를 포함합니다.
+            val results:PendingResult<AutocompletePredictionBuffer>
+                    = Places
+                    .GeoDataApi
+                    .getAutocompletePredictions(googleApiClient, constraint.toString(), bounds, filter)
+
+            // 이 메소드는 기본 UI 스레드에서 호출되어야합니다. API의 결과를 차단하고 최대 60 초 동안 기다립니다.
+            val autocompletePredictions:AutocompletePredictionBuffer = results.await(60, TimeUnit.SECONDS)
+
+            // 쿼리가 성공적으로 완료되었는지 확인하고, 그렇지 않으면 null을 반환합니다.
+            val status:Status = autocompletePredictions.status
+            if(!status.isSuccess) {
+                Log.e("", "Error getting autocomplete prediction API call: $status")
+                autocompletePredictions.release()
+
+                return ArrayList()
+            }
+            Log.i("", "Query completed. Received " + autocompletePredictions.count + " predictions.")
+
+            // 버퍼를 고정 할 수 없으므로 결과를 자체 데이터 구조에 복사합니다.
+            // AutocompletePrediction 객체는 API 응답 (장소 ID 및 설명)을 캡슐화합니다.
+            val iterator:Iterator<AutocompletePrediction> = autocompletePredictions.iterator()
+
+            val resultList:ArrayList<PlaceAutocomplete> = ArrayList(autocompletePredictions.count+1)
+            resultList.add(PlaceAutocomplete("-1", "현위치", "현재주소"))
+            while (iterator.hasNext()) {
+                val prediction = iterator.next()
+                // 세부 정보를 가져 와서 새로운 PlaceAutocomplete 객체로 복사합니다.
+                resultList.add(PlaceAutocomplete(prediction.placeId as CharSequence,
+                        prediction.getPrimaryText(null).toString(), prediction.getFullText(null)))
+            }
+            // 모든 데이터가 복사되었으므로 버퍼를 해제
+            autocompletePredictions.release()
+            return resultList
+        }
+        return ArrayList()
+    }
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): PlaceViewHolder {
+        val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val convertView: View = layoutInflater.inflate(resource, viewGroup, false)
+        val predictionHolder = PlaceViewHolder(convertView)
+
+        return predictionHolder
+    }
+
+    override fun getItemCount(): Int {
+        return resultList.size
+    }
+
+    override fun onBindViewHolder(predictionHolder: PlaceViewHolder, i: Int) {
+        predictionHolder.addressTitle.text = resultList.get(i).title
+        if(resultList.get(i).description.substring(0,4).equals("대한민국"))
+            predictionHolder.addressDetail.text = resultList.get(i).description.substring(5)
+        else
+            predictionHolder.addressDetail.text = resultList.get(i).description
+        predictionHolder.parentLayout.setOnClickListener({
+            listener.onPlaceClick(resultList, i)
+        })
+    }
+
+    class PlaceViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        val parentLayout = itemView.findViewById(R.id.predictedRow) as ConstraintLayout
+        val addressTitle = itemView.findViewById(R.id.tv_address_title) as TextView
+        val addressDetail = itemView.findViewById(R.id.tv_address_detail) as TextView
+    }
+
+    // 지역정보 소유자 데이터 자동완성 API 결과
+    class PlaceAutocomplete(var placeId: CharSequence, var title:CharSequence, var description: CharSequence) {
+
+        override fun toString(): String {
+            return description.toString()
+        }
+    }
+}
