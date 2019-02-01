90e28a22e9a2142c694a76e3261d6844d88336a0
rkddlsgur983 <rkddlsgur983@naver.com>
2019-02-01 15:43:45 +0900

    feat: #25-TabLayout ViewPager 임시적용

---
+++ app/src/main/java/com/nexters/ticktock/DirectionActivity.kt
@@ -0,0 +1,22 @@
+package com.nexters.ticktock
+
+import android.databinding.DataBindingUtil
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.nexters.ticktock.databinding.ActivityDirectionBinding
+
+// TODO 임시적용
+class DirectionActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDirectionBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_direction)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_direction)
+
+        val directionPagerAdapter = DirectionPagerAdapter(supportFragmentManager)
+        binding.viewpagerDirection.adapter = directionPagerAdapter
+    }
+}
---
+++ app/src/main/java/com/nexters/ticktock/DirectionPagerAdapter.kt
@@ -0,0 +1,36 @@
+package com.nexters.ticktock
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+
+class DirectionPagerAdapter: FragmentPagerAdapter {
+
+    var data1: Fragment = Fragment()
+    var data2: Fragment = Fragment()
+
+    var mData: ArrayList<Fragment> = arrayListOf(data1, data2)
+
+    constructor(fm: FragmentManager): super(fm) {
+
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return mData.get(position)
+    }
+
+    override fun getCount(): Int {
+        return mData.size
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        var result: String? = null
+        when (position) {
+            // TODO 대중교통 길찾기이기 때문에 자동차는 없음
+            // 버스 또는 지하철로 필터링은 가능
+            0 -> result = "최단거리"
+            1 -> result = "최소환승"
+        }
+        return result
+    }
+}
