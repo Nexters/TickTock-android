9f0449bb1d1a03a7127b80018f89a0655761da43
Jasonarea <hyeonsuns@naver.com>
2019-02-16 16:28:36 +0900

    #54 - 시스템으로부터 현재시간 자동으로 가져온 후 자동 업데이트

--- app/src/main/java/com/nexters/ticktock/timer/PriorTimerActivity.kt
+++ app/src/main/java/com/nexters/ticktock/timer/PriorTimerActivity.kt
@@ -4,12 +4,21 @@ import android.app.Activity
 import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Window
 import com.nexters.ticktock.R
 import com.nexters.ticktock.databinding.ActivityPriorTimerBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class PriorTimerActivity : AppCompatActivity() {
+
+    var timeHandler : Handler? = null
+    var Runnable : Runnable? = null
+    var isAm : Boolean? = true // true : am, false : pm
 
     private lateinit var binding : ActivityPriorTimerBinding
 
@@ -26,6 +35,28 @@ class PriorTimerActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+
+        //Handler 사용하여 현재 시간 setting하기
+        Runnable  = Runnable {
+            var hourStr = SimpleDateFormat("HH", Locale.KOREA).format(Date())
+            if(hourStr.toInt() > 12) {
+                hourStr = ((hourStr.toInt()) - 12).toString()
+                isAm = false
+            }
+            if(hourStr.toInt() < 10)
+                hourStr = "0$hourStr"
+            val minuteStr = SimpleDateFormat("mm", Locale.KOREA).format(Date())
+            binding.tvTime.text = "$hourStr:$minuteStr"
+
+            binding.tvAmpm.apply {
+                if(isAm!!) text = "AM"
+                else text = "PM"
+            }
+            timeHandler!!.postDelayed(Runnable, 1000)
+        }
+
+        timeHandler = Handler(Looper.getMainLooper())
+        timeHandler!!.postDelayed(Runnable, 10)
     }
 
 }
