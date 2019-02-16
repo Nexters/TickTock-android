3adfc85afab5bced10c559274edce559db2563a1
Jasonarea <hyeonsuns@naver.com>
2019-02-16 15:33:56 +0900

    #54 - x버튼 누르면 홈화면으로 넘어가는 이벤트 구현

--- app/src/main/java/com/nexters/ticktock/timer/PriorTimerActivity.kt
+++ app/src/main/java/com/nexters/ticktock/timer/PriorTimerActivity.kt
@@ -1,8 +1,11 @@
 package com.nexters.ticktock.timer
 
+import android.app.Activity
+import android.content.Intent
 import android.databinding.DataBindingUtil
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
 import com.nexters.ticktock.R
 import com.nexters.ticktock.databinding.ActivityPriorTimerBinding
 
@@ -12,6 +15,17 @@ class PriorTimerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_prior_timer)
+
+
+        binding.btnClose.setOnClickListener {
+            val intent = Intent(Intent.ACTION_MAIN)
+            intent.addCategory(Intent.CATEGORY_HOME)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            startActivity(intent)
+        }
+
     }
+
 }
