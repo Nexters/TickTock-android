b13ab7257e9a4873f7986e517349f98ce806f98e
shouwn <sjonghyon@gmail.com>
2019-02-20 01:47:59 +0900

    DB 개발 완료 sqlite 사용

--- app/src/main/java/com/nexters/ticktock/dao/AlarmDao.kt
+++ app/src/main/java/com/nexters/ticktock/dao/AlarmDao.kt
@@ -1,23 +1,252 @@
 package com.nexters.ticktock.dao
 
-import android.arch.persistence.room.*
-import com.nexters.ticktock.model.entity.Alarm
-import io.reactivex.Flowable
+import android.content.Context
+import android.database.Cursor
+import android.util.Log
+import com.nexters.ticktock.dao.converter.DayConverter
+import com.nexters.ticktock.dao.converter.TickTockColorConverter
+import com.nexters.ticktock.model.Alarm
+import com.nexters.ticktock.model.Step
+import com.nexters.ticktock.utils.Time
 
-@Dao
-interface  AlarmDao {
-    @Query("SELECT * FROM alarm ORDER BY id")
-    fun findAll(): Flowable<Alarm>
+class AlarmDao(private val context: Context) {
 
-    @Query("SELECT * FROM alarm WHERE id = :id")
-    fun findById(id: Long): Flowable<Alarm>
 
-    @Query("SELECT MAX(id) FROM alarm")
-    fun lastAlarmId(): Flowable<Long>
+    private val dayConverter = DayConverter()
+    private val colorConverter = TickTockColorConverter()
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun save(alarm: Alarm): Long
+    companion object {
+        private val SELECT_ALL_ALARM = """
+            SELECT
+                *
+            FROM
+                alarm a
+            LEFT JOIN
+                step s ON a.id = s.alarm_id
+        """.trimIndent()
 
-    @Delete
-    fun delete(alarm: Alarm)
+        private val SELECT_ALL_ALARM_WHERE = """
+            $SELECT_ALL_ALARM
+            WHERE
+                a.id = ?
+        """.trimIndent()
+
+        private val DELETE_ALL_ALARM = """
+            DELETE FROM alarm
+        """.trimIndent()
+
+        private val DELETE_ALARM_WHERE = """
+            $DELETE_ALL_ALARM
+            WHERE id = ?
+        """.trimIndent()
+
+        private val INSERT_ALARM = """
+            INSERT INTO alarm (days, title, start_location, end_location, color, enable, end_time, travel_time)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """.trimIndent()
+
+        private val INSERT_STEP = """
+            INSERT INTO step (name, `order`, duration, alarm_id)
+            VALUES (?, ?, ?, ?)
+        """.trimIndent()
+
+        private val UPDATE_ALARM = """
+            UPDATE alarm
+            SET days = ?,
+                title = ?,
+                start_location = ?,
+                end_location = ?,
+                color = ?,
+                enable = ?,
+                end_time = ?,
+                travel_time = ?
+            WHERE id = ?
+        """.trimIndent()
+
+        private val UPDATE_STEP = """
+            UPDATE step
+            SET name = ?,
+                `order` = ?,
+                duration = ?,
+                alarm_id = ?
+            WHERE id = ?
+        """.trimIndent()
+
+        private val MAX_ALARM_ID = """
+            SELECT
+	            MAX(id)
+            FROM
+	            alarm
+        """.trimIndent()
+
+        private val COUNT_ALARM = """
+            SELECT
+                COUNT(id)
+            FROM
+                alarm
+        """.trimIndent()
+    }
+
+    fun getAlarmCount(): Long {
+        val readableDB = TickTockDBHelper.getInstance(context).readableDatabase
+
+        return readableDB.rawQuery(COUNT_ALARM, null).use { it.getLong(0) }
+    }
+
+    fun findAll(): List<Alarm> {
+        val readableDB = TickTockDBHelper.getInstance(context).readableDatabase
+        val cursor = readableDB.rawQuery(SELECT_ALL_ALARM, null)
+
+        val alarmMap = mutableMapOf<Long, Alarm>()
+        val stepSet = mutableSetOf<Step>()
+
+        cursor.use {
+            if (it.moveToFirst()) {
+                do {
+                    it.makePair().also {pair ->
+                        alarmMap[pair.first.id] = pair.first
+                        pair.second?.let { it1 -> stepSet.add(it1) }
+                    }
+                } while (it.moveToNext())
+            }
+        }
+
+        val alarmList = stepSet.groupBy { it.alarmId }
+                .map {
+                    alarmMap[it.key]?.apply {
+                        steps.addAll(it.value)
+                    }
+                }.filterNotNull()
+
+        return if (alarmList.isEmpty()) {
+            alarmMap.values.toList()
+        } else {
+            alarmList
+        }
+    }
+
+    fun findById(id: Long): Alarm? {
+        val readableDB = TickTockDBHelper.getInstance(context).readableDatabase
+        val cursor = readableDB.rawQuery(SELECT_ALL_ALARM_WHERE, null)
+
+        var alarm: Alarm? = null
+        val stepSet = mutableSetOf<Step>()
+
+        cursor.use {
+            if (it.moveToFirst()) {
+                do {
+                    it.makePair().also {pair ->
+                        alarm = pair.first
+                        pair.second?.let { it1 -> stepSet.add(it1) }
+                    }
+                } while (it.moveToNext())
+            }
+        }
+
+        return alarm?.apply { steps = stepSet }
+    }
+
+    fun save(alarm: Alarm) {
+
+        Log.d("database", "save")
+        if (alarm.id != 0L) {
+            update(alarm)
+        } else {
+            insert(alarm)
+        }
+    }
+
+    private fun insert(alarm: Alarm) {
+        val writableDB = TickTockDBHelper.getInstance(context).writableDatabase
+        val readableDB = TickTockDBHelper.getInstance(context).readableDatabase
+
+        writableDB.execSQL(INSERT_ALARM, arrayOf(
+                alarm.days.let { dayConverter.toDatabase(it) },
+                alarm.title,
+                alarm.startLocation,
+                alarm.endLocation,
+                alarm.color.name,
+                alarm.enable.let { if (it) 1 else 0 },
+                alarm.endTime.time,
+                alarm.travelTime.time
+        ))
+
+        val alarmId = readableDB.rawQuery(MAX_ALARM_ID, null).use {
+            it.moveToFirst()
+            it.getLong(0)
+        } + 1
+        val steps = alarm.steps
+
+        steps.forEach {
+            writableDB.execSQL(INSERT_STEP, arrayOf(
+                    it.name,
+                    it.order,
+                    it.duration,
+                    alarmId
+            ))
+        }
+    }
+
+    private fun update(alarm: Alarm) {
+        val writableDB = TickTockDBHelper.getInstance(context).writableDatabase
+        writableDB.execSQL(UPDATE_ALARM, arrayOf(
+                alarm.days.let { dayConverter.toDatabase(it) },
+                alarm.title,
+                alarm.startLocation,
+                alarm.endLocation,
+                alarm.color.name,
+                alarm.enable.let { if (it) 1 else 0 },
+                alarm.endTime.time,
+                alarm.travelTime.time,
+                alarm.id
+        ))
+
+        alarm.steps.forEach {
+            writableDB.execSQL(UPDATE_STEP, arrayOf(
+                    it.name,
+                    it.order,
+                    it.duration,
+                    alarm.id,
+                    it.id
+            ))
+        }
+    }
+
+    fun delete(alarm: Alarm) {
+        val writableDB = TickTockDBHelper.getInstance(context).writableDatabase
+        writableDB.execSQL(DELETE_ALARM_WHERE, arrayOf(alarm.id))
+    }
+
+    fun deleteAll() {
+        val writableDB = TickTockDBHelper.getInstance(context).writableDatabase
+        writableDB.execSQL(DELETE_ALL_ALARM)
+    }
+
+    private fun Cursor.makePair(): Pair<Alarm, Step?> {
+        val alarm = Alarm(
+                id = getLong(0),
+                days = getString(1).let { dayConverter.fromDatabase(it) },
+                title = getString(2),
+                startLocation = getString(3),
+                endLocation = getString(4),
+                color = getString(5).let { colorConverter.fromDatabase(it) },
+                enable = getInt(6) != 0,
+                endTime = Time(getInt(7)),
+                travelTime = Time(getInt(8))
+        )
+
+        if (getLong(9) == 0L) {
+            return alarm to null
+        }
+
+        val step = Step(
+                id = getLong(9),
+                name = getString(10),
+                order = getInt(11),
+                duration = Time(getInt(12)),
+                alarmId = getLong(13)
+        )
+
+        return alarm to step
+    }
 }
