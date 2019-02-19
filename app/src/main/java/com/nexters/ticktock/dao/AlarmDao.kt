812db5d97971fe24e2309eb5b75800cdd238dd0d
shouwn <sjonghyon@gmail.com>
2019-02-19 20:57:34 +0900

    색상 및 그림 최신화

--- app/src/main/java/com/nexters/ticktock/dao/AlarmDao.kt
+++ app/src/main/java/com/nexters/ticktock/dao/AlarmDao.kt
@@ -1,11 +1,23 @@
 package com.nexters.ticktock.dao
 
-import android.arch.persistence.room.Dao
-import android.arch.persistence.room.Query
+import android.arch.persistence.room.*
 import com.nexters.ticktock.model.entity.Alarm
+import io.reactivex.Flowable
 
 @Dao
-interface  AlarmDao : BaseDao<Alarm> {
-    @Query("SELECT * FROM alarm")
-    fun findAll(): Array<Alarm>
+interface  AlarmDao {
+    @Query("SELECT * FROM alarm ORDER BY id")
+    fun findAll(): Flowable<Alarm>
+
+    @Query("SELECT * FROM alarm WHERE id = :id")
+    fun findById(id: Long): Flowable<Alarm>
+
+    @Query("SELECT MAX(id) FROM alarm")
+    fun lastAlarmId(): Flowable<Long>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun save(alarm: Alarm): Long
+
+    @Delete
+    fun delete(alarm: Alarm)
 }
