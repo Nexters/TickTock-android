e862548b934a7a8ef9ebe0df56e769995836315b
rkddlsgur983 <rkddlsgur983@naver.com>
2019-02-22 22:45:32 +0900

    feat: #107 apply db main detail

--- app/src/main/java/com/nexters/ticktock/MainDetailActivity.kt
+++ app/src/main/java/com/nexters/ticktock/MainDetailActivity.kt
@@ -5,9 +5,11 @@ import android.content.Intent
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.TimePicker
-import android.widget.Toast
 import com.nexters.ticktock.Constant.GPS_ENABLE_REQUEST_CODE
 import com.nexters.ticktock.Constant.MAIN_DETAIL_ACTIVITY_REQUEST_CODE
 import com.nexters.ticktock.alarmsetting.AlarmSettingSecondActivity
@@ -15,16 +17,133 @@ import com.nexters.ticktock.autocomplete.AutoCompleteActivity
 import com.nexters.ticktock.dao.TickTockDBHelper
 import com.nexters.ticktock.databinding.ActivityMainDetailBinding
 import com.nexters.ticktock.model.Alarm
+import com.nexters.ticktock.model.Step
 import com.nexters.ticktock.model.enums.Day
 import com.nexters.ticktock.model.enums.TickTockColor
 import com.nexters.ticktock.utils.Location
 import com.nexters.ticktock.utils.Time
 import com.nexters.ticktock.utils.getResizedString
 import com.nexters.ticktock.utils.getUnderlinedString
+import java.util.*
 
-class MainDetailActivity(): AppCompatActivity(), View.OnClickListener {
+class MainDetailActivity: AppCompatActivity(), View.OnClickListener {
 
-    constructor(id: Long): this() {
+    private lateinit var binding: ActivityMainDetailBinding
+
+    private var alarmId: Long = 0
+    private var startLocation = ""
+    private var endLocation = ""
+    private var travelTime = Time(0)
+    private var hour = 0
+    private var minute = 0
+    private var startHour = 0
+    private var startMinute = 0
+    private var dayList: EnumSet<Day> = EnumSet.noneOf(Day::class.java)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main_detail)
+
+        Location.getInstance(this)
+        Location.getInstance(this).isGPSConnected()
+
+        getData(intent.getLongExtra("CARD_ID", 0))
+
+        binding.tvAlarmTime.text = getResizedString("*11:30* AM", 3.125f)
+        startLocation = "현위치"
+        binding.tvDeliveryFromTitle.text = getUnderlinedString("*${startLocation}*")
+        endLocation = "종각역 마이크임팩트"
+        binding.tvDeliveryToTitle.text = getUnderlinedString("*${endLocation}*")
+
+        binding.btnClose.setOnClickListener(this)
+        binding.btnEdit.setOnClickListener(this)
+        binding.btnEditPrepareTime.setOnClickListener(this)
+        binding.layoutSave.setOnClickListener(this)
+
+        binding.tpMeetingTime.setOnTimeChangedListener(object: TimePicker.OnTimeChangedListener{
+            override fun onTimeChanged(view: TimePicker?, hourOfDay: Int, minute: Int) {
+                startHour = hourOfDay
+                startMinute = minute
+            }
+        })
+
+        binding.edMemo.addTextChangedListener(object: TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (count > 0) {
+                    binding.tvMemoMax.visibility = View.GONE
+                    binding.layoutSave.isEnabled = true
+                    binding.layoutSave.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.btnEnable))
+                }
+                else if (count == 0) {
+                    binding.tvMemoMax.visibility = View.VISIBLE
+                    binding.layoutSave.isEnabled = false
+                    binding.layoutSave.setBackgroundColor(ContextCompat.getColor(applicationContext, R.color.btnDisEnable))
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            // gps 설정 변경 후 재연결
+            GPS_ENABLE_REQUEST_CODE -> {
+                if (Location.getInstance(this).isGPSConnected())
+                    Location.getInstance(this).getLocation()
+            }
+
+            MAIN_DETAIL_ACTIVITY_REQUEST_CODE -> {
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        hour = data?.getIntExtra("DIRECTION_TIME_HOUR", 0) as Int
+                        if (hour != 0) {
+                            minute = data?.getIntExtra("DIRECTION_TIME_MINUTE", 0) as Int
+                            binding.tvDeliveryTimeSecond.setText("${hour}시간 ${minute}분")
+                            travelTime = Time(hour * 60 + minute)
+                        }
+                        else {
+                            minute = data.getIntExtra("DIRECTION_TIME_MINUTE", 0)
+                            binding.tvDeliveryTimeSecond.setText("${minute}분")
+                        }
+                        startLocation = "${data?.getStringExtra("DIRECTION_START")}"
+                        binding.tvDeliveryFromTitle.text = getUnderlinedString("*${startLocation}*")
+                        endLocation = "${data?.getStringExtra("DIRECTION_DESTINATION")}"
+                        binding.tvDeliveryToTitle.text = getUnderlinedString("*${endLocation}*")
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            binding.btnClose.id -> finish()
+
+            binding.btnEdit.id -> {
+                val intent = Intent(this, AutoCompleteActivity::class.java)
+                intent.putExtra("GPS_RESULT", Location.getInstance(this).getResult())
+                intent.putExtra("DESTINATION_DATA", binding.tvDeliveryToTitle.text)
+                startActivityForResult(intent, MAIN_DETAIL_ACTIVITY_REQUEST_CODE)
+            }
+
+            binding.btnEditPrepareTime.id -> {
+                val intent = Intent(this, AlarmSettingSecondActivity::class.java)
+                startActivity(intent)
+            }
+
+            binding.layoutSave.id -> {
+                setData()
+                finish()
+            }
+        }
+    }
+
+    fun getData(id: Long) {
+        alarmId = id
         val alarmDao = TickTockDBHelper.getInstance(this).alarmDao
         val alarm = alarmDao.findById(id) as Alarm
         val prepareTime = alarm.steps.map { it.duration }.fold(Time(0)) { acc, time -> acc + time }
@@ -34,13 +153,18 @@ class MainDetailActivity(): AppCompatActivity(), View.OnClickListener {
         binding.tvDestination.text = alarm.endLocation
         binding.tvDay.text = alarm.days.toString()
 
-        if (alarm.travelTime.hour != 0) binding.tvDeliveryTimeSecond.text = "${alarm.travelTime.hour}시간 ${alarm.travelTime.minute}분"
-        else binding.tvDeliveryTimeSecond.text = "${alarm.travelTime.minute}분"
+        hour = alarm.travelTime.hour
+        minute = alarm.travelTime.minute
+
+        if (hour != 0) binding.tvDeliveryTimeSecond.text = "${hour}시간 ${minute}분"
+        else binding.tvDeliveryTimeSecond.text = "${minute}분"
         binding.tvDeliveryFromTitle.text = getUnderlinedString("*${alarm.startLocation}*")
         binding.tvDeliveryToTitle.text = getUnderlinedString("*${alarm.endLocation}*")
 
-        binding.tpMeetingTime.setCurrentHour(startTime.hour)
-        binding.tpMeetingTime.setCurrentMinute(startTime.minute)
+        startHour = startTime.hour
+        startMinute = startTime.minute
+        binding.tpMeetingTime.setCurrentHour(startHour)
+        binding.tpMeetingTime.setCurrentMinute(startMinute)
 
         alarm.days.forEach {
             val checkbox = when (it) {
@@ -64,101 +188,43 @@ class MainDetailActivity(): AppCompatActivity(), View.OnClickListener {
             TickTockColor.PURPLE -> binding.radiogrpColor.check(binding.radiobtn5.id)
         }
 
-        binding.cbMonday
-        binding.cbMonday
-
         if (prepareTime.hour != 0) binding.tvPrepareTimeSecond.text = "${prepareTime.hour}시간 ${prepareTime.minute}분"
         else binding.tvPrepareTimeSecond.text = "${prepareTime.minute}분"
 
         binding.edMemo.setText(alarm.title)
     }
 
-    private lateinit var binding: ActivityMainDetailBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main_detail)
-
-        Location.getInstance(this)
-        Location.getInstance(this).isGPSConnected()
-
-        binding.tvAlarmTime.text = getResizedString("*11:30* AM", 3.125f)
-        binding.tvDeliveryFromTitle.text = getUnderlinedString("*현위치*")
-        binding.tvDeliveryToTitle.text = getUnderlinedString("*종각역 마이크임팩트*")
-
-        binding.btnClose.setOnClickListener(this)
-        binding.btnEdit.setOnClickListener(this)
-        binding.btnEditPrepareTime.setOnClickListener(this)
-        binding.btnSave.setOnClickListener(this)
-
-        binding.tpMeetingTime.setOnTimeChangedListener(object: TimePicker.OnTimeChangedListener{
-            override fun onTimeChanged(view: TimePicker?, hourOfDay: Int, minute: Int) {
-                // TODO data set
-            }
-        })
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            // gps 설정 변경 후 재연결
-            GPS_ENABLE_REQUEST_CODE -> {
-                if (Location.getInstance(this).isGPSConnected())
-                    Location.getInstance(this).getLocation()
-            }
-
-            MAIN_DETAIL_ACTIVITY_REQUEST_CODE -> {
-                when (resultCode) {
-                    Activity.RESULT_OK -> {
-                        if (data?.getIntExtra("DIRECTION_TIME_HOUR", 0) != 0) binding.tvDeliveryTimeSecond.setText("${data?.getIntExtra("DIRECTION_TIME_HOUR", 0)}시간 ${data?.getIntExtra("DIRECTION_TIME_MINUTE", 0)}분")
-                        else binding.tvDeliveryTimeSecond.setText("${data.getIntExtra("DIRECTION_TIME_MINUTE", 0)}분")
-                        binding.tvDeliveryFromTitle.text = getUnderlinedString("*${data?.getStringExtra("DIRECTION_START")}*")
-                        binding.tvDeliveryToTitle.text = getUnderlinedString("*${data?.getStringExtra("DIRECTION_DESTINATION")}*")
-                    }
-                }
-            }
-        }
-    }
-
-    override fun onClick(v: View?) {
-        when (v?.id) {
-            binding.btnClose.id -> finish()
-
-            binding.btnEdit.id -> {
-                val intent = Intent(this, AutoCompleteActivity::class.java)
-                intent.putExtra("GPS_RESULT", Location.getInstance(this).getResult())
-                intent.putExtra("DESTINATION_DATA", binding.tvDeliveryToTitle.text)
-                startActivityForResult(intent, MAIN_DETAIL_ACTIVITY_REQUEST_CODE)
-            }
-
-            binding.btnEditPrepareTime.id -> {
-                val intent = Intent(this, AlarmSettingSecondActivity::class.java)
-                startActivity(intent)
-            }
-
-            binding.btnSave.id -> {
-                // TODO save
-                //finish()
-            }
-        }
-    }
-
     fun setData() {
 
-        binding.cbMonday.isChecked
-        binding.cbTuesday.isChecked
-        binding.cbWednesday.isChecked
-        binding.cbThursday.isChecked
-        binding.cbFriday.isChecked
-        binding.cbSaturday.isChecked
-        binding.cbSunday.isChecked
+        if (binding.cbMonday.isChecked) dayList.add(Day.Monday)
+        if (binding.cbTuesday.isChecked) dayList.add(Day.Tuesday)
+        if (binding.cbWednesday.isChecked) dayList.add(Day.Wednesday)
+        if (binding.cbThursday.isChecked) dayList.add(Day.Thursday)
+        if (binding.cbFriday.isChecked) dayList.add(Day.Friday)
+        if (binding.cbSaturday.isChecked) dayList.add(Day.Saturday)
+        if (binding.cbSunday.isChecked) dayList.add(Day.Sunday)
 
+        var color = TickTockColor.RED
         when (binding.radiogrpColor.checkedRadioButtonId) {
-            binding.radiobtn1.id -> Toast.makeText(this, "1", Toast.LENGTH_LONG).show()
-            binding.radiobtn2.id -> Toast.makeText(this, "2", Toast.LENGTH_LONG).show()
-            binding.radiobtn3.id -> Toast.makeText(this, "3", Toast.LENGTH_LONG).show()
-            binding.radiobtn4.id -> Toast.makeText(this, "4", Toast.LENGTH_LONG).show()
-            binding.radiobtn5.id -> Toast.makeText(this, "5", Toast.LENGTH_LONG).show()
+            binding.radiobtn1.id -> color = TickTockColor.RED
+            binding.radiobtn2.id -> color = TickTockColor.GREEN
+            binding.radiobtn3.id -> color = TickTockColor.YELLOW
+            binding.radiobtn4.id -> color = TickTockColor.BLUE
+            binding.radiobtn5.id -> color = TickTockColor.PURPLE
         }
+
+        val alarmDao = TickTockDBHelper.getInstance(this).alarmDao
+
+        alarmDao.save(Alarm(
+                id = alarmId,
+                days = dayList,
+                title = binding.edMemo.text.toString(),
+                startLocation = startLocation,
+                endLocation = endLocation,
+                color = color,
+                enable = true,
+                endTime = Time(startHour * 60 + startMinute),
+                travelTime = Time(hour * 60 + minute)
+        ))
     }
 }
